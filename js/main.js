4978f188e7d53718a4a9d091f3a01d04a6c7f017
Abdulaz1m <azim.kojoev1987@gmail.com>
2022-04-19 22:25:35 +0300

    game

--- js/main.js
+++ js/main.js
@@ -49,14 +49,7 @@ menu__calls.addEventListener('click', function () {
     document.querySelector('.whats__body1').classList.add('whats__left')
 })
 
-//объекты
-const obj1 = {
-    photo: "img/whats_ava1.jpg"
-}
-
 //новый чат
-const msg_audio = document.getElementById('whats_msg')
-
 function msgAmount () {
     document.querySelector('.whats__chat-new').classList.remove('none')
     document.querySelector('.menu__new-msg').classList.remove('none')
@@ -67,75 +60,59 @@ function msgAmount () {
 function writeSomeone () {
     document.querySelector('.whats__body1').classList.add('block')
     document.querySelector('.no__chat').classList.add('none')
-    // document.querySelector('.chat__name').textContent = 'Неизвестный'
-    // document.querySelector('.chat__time').textContent = 'В сети'
-    // document.querySelector('.chat__date').textContent = 'Сегодня'
-    // if (!document.querySelector('.msg1_1')){
-    //     document.querySelector('.chat__body').innerHTML += `<p class="msg msg1 msg1_1">Привет</p>`
-    // }
     document.querySelector('.menu__new-msg').classList.add('flex')
-    document.querySelector('.whats__chats').innerHTML += `
-    <div data-photo="${obj1.photo}" data-name="Неизвестный" class="whats__chat whats__chat1">
+    document.querySelector('.whats__chat1').innerHTML += `
         <img src="img/whats_ava1.jpg" alt="ava">
         <div class="whats__chat-info">
             <h3>Неизвестный</h3>
             <p>Привет</p>
         </div>
-        <div class="whats__chat-new none"></div>
+        <div class="whats__chat-new whats__chat-new1 none"></div>
         <span class="whats__chat-time"></span>
-    </div>
     `
-    msgAmount()
-    newChat()
-}
-setTimeout(writeSomeone, 1 * 1000)
-
-//открыть/закрыть чат
-function newChat () {
-    let chats = document.querySelectorAll('.whats__chat')
-    chats.forEach(chat => chat.addEventListener('click', openChat))
-    function openChat (e) {
-        e.preventDefault();
-        const dataset = e.target.dataset
-        const data = {
-            photo: dataset.photo,
-        }
+    function chat1 () {
+        document.querySelector('.whats-chat__body1').innerHTML += `
+            <div class="chat__menu">
+                <button class="chat__exit chat__exit1">
+                    <img class="chat__back" src="img/arrow_left.svg" alt="arrow_left">
+                </button>                           
+                <img class="chat__ava" src="img/whats_ava1.jpg" alt="chat__ava">
+                <div class="chat__info">
+                    <h4 class="chat__name">Неизвестный</h4>
+                    <p class="chat__time">В сети</p>
+                </div>
+            </div>
+            <div class="chat__body">
+                <div class="chat__date">Сегодня</div>
+            </div>
+            <div class="chat__text">
+                <input placeholder="Сообщение" type="text" class="text">
+                <button class="text__send">
+                    <img src="img/whats_send.png" alt="whats_send">
+                </button>
+            </div>   
+        `    
+    }
+     function openChat1() {
         document.querySelector('.whats__main').classList.add('whats__left')
-        document.querySelector('.whats-chat__body').classList.add('whats__left2')
-        document.querySelector('.whats__chat-new').classList.add('none')
+        document.querySelector(`.whats-chat__body1`).classList.add('whats__left2')
+        document.querySelector('.whats__chat-new1').classList.add('none')
         document.querySelector('.menu__new-msg').textContent -= document.querySelectorAll('.msg1').length
         if (document.querySelector('.menu__new-msg').textContent = '0'){
             document.querySelector('.menu__new-msg').classList.add('none')
         }
-        if (!document.querySelector('.whats-chat__body1')){
-            document.querySelector('.whats-chat__body').innerHTML += `
-                <div class="whats-chat__body1">
-                    <div class="chat__menu">
-                        <button onclick="exitChat()" class="chat__exit">
-                            <img class="chat__back" src="img/arrow_left.svg" alt="arrow_left">
-                        </button>                           
-                        <img class="chat__ava" src="${e.target.dataset.photo}" alt="chat__ava">
-                        <div class="chat__info">
-                            <h4 class="chat__name"></h4>
-                            <p class="chat__time"></p>
-                        </div>
-                    </div>
-                    <div class="chat__body">
-                        <div class="chat__date"></div>
-                    </div>
-                    <div class="chat__text">
-                        <input placeholder="Сообщение" type="text" class="text">
-                        <button class="text__send">
-                            <img src="img/whats_send.png" alt="whats_send">
-                        </button>
-                    </div>
-                </div>
-            `
-        }
     }
+    function exitChat () {
+       document.querySelector('.whats__main').classList.remove('whats__left')
+        document.querySelector('.whats-chat__body1').classList.remove('whats__left2')
+    }
+    chat1()
+    msgAmount()
+    document.querySelector('.whats__chat1').addEventListener('click', openChat1)
+    document.querySelector('.chat__exit1').addEventListener('click', exitChat)
 }
-function exitChat () {
-    document.querySelector('.whats__main').classList.remove('whats__left')
-    document.querySelector('.whats-chat__body').classList.remove('whats__left2')
-}
+setTimeout(writeSomeone, 1 * 1000)
+
+
+
 
